a084a56f09b9f0a45acbc0252ef62969d6fd5e8f
Moti Zilberman <moti@fb.com>
2025-08-18 17:08:24 +0100

    chore: move npm publishing into release.yml

--- node/platforms.js
+++ node/platforms.js
@@ -8,29 +8,36 @@
  */
 
 module.exports = {
+  // Keep in sync with .github/workflows/release.yml - the 'npm-publish' job's dependencies
+  // MUST include the build job for each artifact listed below.
   artifactsByPlatformAndArch: {
     linux: {
       arm64: {
+        // Build job: 'linux-musl-arm64'
         slug: 'linux-musl.aarch64',
         binary: 'dotslash',
       },
       x64: {
+        // Build job: 'linux-musl-x86_64'
         slug: 'linux-musl.x86_64',
         binary: 'dotslash',
       },
     },
     darwin: {
       '*': {
+        // Build job: 'macos'
         slug: 'macos',
         binary: 'dotslash',
       },
     },
     win32: {
       arm64: {
+        // Build job: 'windows-arm64'
         slug: 'windows-arm64',
         binary: 'dotslash.exe',
       },
       x64: {
+        // Build job: 'windows'
         slug: 'windows',
         binary: 'dotslash.exe',
       },
